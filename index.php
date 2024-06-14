2c0d330331214779402d8196c4b32c63496b6165
DanielLopez <danieldejesuslopezperez859@gmail.com>
2024-06-14 10:30:31 -0600

    Operaciones de suma y resta

--- index.php
+++ index.php
@@ -33,8 +33,6 @@
                                 <select id="operacion" name="operacion" class="form-control" required>
                                     <option value="suma" <?php echo isset($_POST['operacion']) && $_POST['operacion'] == 'suma' ? 'selected' : ''; ?>>Suma</option>
                                     <option value="resta" <?php echo isset($_POST['operacion']) && $_POST['operacion'] == 'resta' ? 'selected' : ''; ?>>Resta</option>
-                                    <option value="multiplicacion" <?php echo isset($_POST['operacion']) && $_POST['operacion'] == 'multiplicacion' ? 'selected' : ''; ?>>Multiplicación</option>
-                                    <option value="division" <?php echo isset($_POST['operacion']) && $_POST['operacion'] == 'division' ? 'selected' : ''; ?>>División</option>
                                 </select>
                             </div>
                             <button type="submit" class="btn btn-primary btn-block">Calcular</button>
@@ -52,18 +50,7 @@
                                     break;
                                 case 'resta':
                                     $resultado = $numero1 - $numero2;
-                                    break;
-                                case 'multiplicacion':
-                                    $resultado = $numero1 * $numero2;
-                                    break;
-                                case 'division':
-                                    if ($numero2 != 0) {
-                                        $resultado = $numero1 / $numero2;
-                                    } else {
-                                        echo "<div class='alert alert-danger mt-3'>No se puede dividir por cero</div>";
-                                        exit();
-                                    }
-                                    break;
+                                    break;  
                                 default:
                                     echo "<div class='alert alert-danger mt-3'>Operación no válida</div>";
                                     exit();
